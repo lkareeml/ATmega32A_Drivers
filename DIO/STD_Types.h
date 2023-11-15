106da3b72bcf3e81dc263d70e6bf58ff0d3cd17a
kareem atef <kareem.atef@yahoo.com>
2023-11-15 03:52:00 +0200

    Create STD_Types.h

---
+++ DIO/STD_Types.h
@@ -0,0 +1,17 @@
+/****************************************/
+/*          Author: Kareem Atef		    */
+/*          Date: 10-11-2023		    */
+/*          Version: 1.0				*/
+/*          Module: STD Types           */
+/****************************************/
+
+#ifndef STD_H_INCLUDED
+#define STD_H_INCLUDED
+typedef unsigned char uint8;
+typedef char int8;
+typedef unsigned short uint16;
+typedef short int16;
+typedef unsigned int uint32;
+typedef int int32;
+typedef enum {false, true} bool;
+#endif
